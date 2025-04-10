0df5e0c66507f1bf60a8a0321a15f93cc7d6ae2b
Claudia <claudialo.garciar@gmail.com>
2025-04-09 21:34:59 -0500

    reto cumplido

---
+++ Repaso_Modulo1/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Repaso_Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======================");
+            Console.WriteLine("Repaso bucles");
+
+            /*
+             * 
+             * * 
+             * * * 
+             * * * *
+             * * * * *
+             */
+            int i, j, filas;
+            Console.WriteLine("Ingrese el número de filas");
+            filas = Convert.ToInt32(Console.ReadLine());    
+
+            for ( i = 1; i <= filas; i++) {
+
+                for ( j = 1; j <= i; j++)
+                {
+                    
+                        Console.Write('*');
+
+                }
+                Console.Write('\n');
+
+            }
+
+
+            for (int k = 1; k <= filas; k++)
+            {
+
+                for (int l = 1; l <= k; l++)
+                {
+
+                    Console.Write(l);
+
+                }
+                Console.Write('\n');
+
+            }
+
+
+            //for (int k = 5; k >= 1; k--)
+            //{
+
+            //    for (int l = 1; l <= 5; l++)
+            //    {
+
+            //        if (k == l)
+            //        {
+            //            Console.WriteLine('*' + " ");
+            //            break;
+            //        }
+            //        Console.Write('*' + " ");
+            //    }
+
+            //}
+
+        }
+    }
+}
